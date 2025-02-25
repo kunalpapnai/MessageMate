57e1e4aeaf1bdd05a690d287cbd156990253f366
Kunal Papnai <kunalpapnai123@gmail.com>
2025-02-25 15:17:20 +0530

    ui fixes

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,13 @@
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import {BrowserRouter} from "react-router-dom";
-import Routing_App from './POC/Protected_Routing/Routing_App.jsx';
-import User from './POC/useEffect/User.jsx';
-import PropDrilling from './Context/PropDrilling.tsx';
-import PropDrillingSol from './Context/PropDrillingSol.tsx';
-import Home from './Context/ThemeChanger/Home.jsx';
-import ThemeWrapper from './Context/ThemeChanger/ThemeContext.jsx';
 import App2 from './App2.jsx';
 import AuthWrapper from './Components/AuthContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <AuthWrapper>
     <BrowserRouter>
-  <App2></App2>
-    {/* <ThemeWrapper>
-      <App />
-    </ThemeWrapper> */}
-    {/* <Routing_App></Routing_App> */}
-    {/* <User></User> */}
-    {/* <PropDrilling></PropDrilling> */}
-    {/* <PropDrillingSol></PropDrillingSol> */}
-  </BrowserRouter>
+      <App2></App2>
+    </BrowserRouter>
   </AuthWrapper>
 )
